39855938b03643be6be53ccfebedb6ae7a81e070
Ren Carothers <rencarothers@gmail.com>
2020-12-10 14:50:33 -0800

    refines psuedocode

--- src/is_pangram.js
+++ src/is_pangram.js
@@ -1,10 +1,43 @@
 const isPangram = function(text) {
-    const letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+    const letters = {
+        'a': 0, 
+        'b': 0,
+        'c': 0, 
+        'd': 0, 
+        'e': 0, 
+        'f': 0, 
+        'g': 0, 
+        'h': 0, 
+        'i': 0, 
+        'j': 0, 
+        'k': 0, 
+        'l': 0, 
+        'm': 0, 
+        'n': 0, 
+        'o': 0, 
+        'p': 0, 
+        'q': 0, 
+        'r': 0, 
+        's': 0,
+        't': 0, 
+        'u': 0, 
+        'v': 0, 
+        'w': 0, 
+        'x': 0, 
+        'y': 0, 
+        'z': 0
+    }
 
-    // text must include all the things in the array
-    // can have other stuff I guess
-    // want it to return true or false
-    return false
+    for (letter in letters) {
+        // if letter is inside the letter hash, add one to that key's value 
+    }
+
+    // after the loop ends, make sure all keys have at least a one, return true if so
+
+    // otherwise, return false
+
+
+    return true
 };
 
 module.exports = isPangram;
