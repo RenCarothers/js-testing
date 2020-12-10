40fa17a6bf135ec44853f0b12557f1823cdd5d79
Ren Carothers <rencarothers@gmail.com>
2020-12-10 14:34:36 -0800

    creates basic tests, begins tdd cycle: psuedocode-red ...next: turn green

--- src/is_pangram.js
+++ src/is_pangram.js
@@ -1,5 +1,10 @@
 const isPangram = function(text) {
+    const letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
+    // text must include all the things in the array
+    // can have other stuff I guess
+    // want it to return true or false
+    return false
 };
 
 module.exports = isPangram;
--- test/is_pangram.test.js
+++ test/is_pangram.test.js
@@ -10,54 +10,64 @@ describe('isPangram()', () => {
     const text = 'the quick brown fox jumps over the lazy dog';
 
     // Act
-
+    isPangram(text);
     // Assert
+    expect(isPangram()).toEqual(true)
   });
 
   test('works with "abcdefghijklmnopqrstuvwxyz"', () => {
     // Arrange
-
+    const text = 'abcdefghijklmnopqrstuvwxyz';
     // Act
-
+    isPangram(text);
     // Assert
-
+    expect(isPangram()).toEqual(true)
   });
 
   test("missing character 'x'", () => {
     // Arrange
-
+    const text = 'abcdefghijklmnopqrstuvwyz';
     // Act
-
+    isPangram(text);
     // Assert
-
+    expect(isPangram()).toEqual(true)
   });
 
   test('empty sentence', () => {
     // Arrange
-
+    const text = '';
     // Act
-
+    isPangram(text);
     // Assert
-
+    expect(isPangram()).toEqual(true)
   });
 
   test('pangram with underscores instead of spaces works', () => {
     // Arrange
-
+    const text = 'the_quick_brown_fox_jumps over_the_lazy_dog';
     // Act
-
+    isPangram(text);
     // Assert
-
+    expect(isPangram()).toEqual(true)
   });
 
   test('pangram with numbers', () => {
     // Arrange
-
-    // Act
-
-    // Assert
-
+    const text = '01 the quick brown fox 77 jumps over the lazy dog 23';
+   // Act
+   isPangram(text);
+   // Assert
+   expect(isPangram()).toEqual(true)
   });
 
   // Write your own test case
+
+  test('panagram with nonnumeric characters', () => {
+    // Arrange
+    const text = '%$ the quick brown fox ^@ jumps over the lazy dog *#';
+    // Act
+    isPangram(text);
+    // Assert
+    expect(isPangram()).toEqual(true)
+  });
 });
